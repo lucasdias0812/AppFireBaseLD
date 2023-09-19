a861c0c52a82489133cbc243def951c634b16cc7
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2023-09-19 13:40:04 -0300

    Add files via upload

---
+++ src/app/models/Produto.ts
@@ -0,0 +1,5 @@
+export class Produto{
+  id= '';
+  nome= '';
+  valor= 0
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyBy_DDFDON1Pff_Ocn9LDRh12EI7mfvVio',
+    authDomain: 'ionicfirebaseapp-b3dea.firebaseapp.com',
+    projectId: 'ionicfirebaseapp-b3dea',
+    storageBucket: 'ionicfirebaseapp-b3dea.appspot.com',
+    messagingSenderId: '277876462913',
+    appId: '1:277876462913:web:dac8119d2474975fc3bdc0',
+    measurementId: 'G-VTRH2CDNXS'
+  }
+};
